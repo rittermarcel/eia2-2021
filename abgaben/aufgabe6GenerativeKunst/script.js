0171334a2349c6ce04883e0d539e4fedfb4d4a53
Marcel Ritter <64130092+rittermarcel@users.noreply.github.com>
2021-05-08 10:00:54 +0200

    aufgabe6

---
+++ abgaben/aufgabe6GenerativeKunst/script.js
@@ -0,0 +1,47 @@
+"use strict";
+var GenerativeKunst;
+(function (GenerativeKunst) {
+    console.log("start");
+    window.addEventListener("load", handleLoad);
+    function handleLoad(_event) {
+        let canvas = document.querySelector("canvas");
+        let crc2 = canvas.getContext("2d");
+        crc2.canvas.width = canvas.clientWidth;
+        crc2.canvas.height = canvas.clientHeight;
+        document.getElementById("refresh")?.addEventListener("click", refresh);
+        draw(crc2);
+    }
+    function draw(crc2) {
+        crc2.fillStyle = "#98ff98";
+        crc2.fillRect(0, 0, crc2.canvas.width, crc2.canvas.height);
+        for (let i = 0; i < 100; i++) {
+            let x = Math.floor(Math.random() * crc2.canvas.width);
+            let y = Math.floor(Math.random() * crc2.canvas.height);
+            let radius = Math.floor(Math.random() * 20) + 10;
+            let r = Math.floor(Math.random() * 255);
+            let g = Math.floor(Math.random() * 255);
+            let b = Math.floor(Math.random() * 255);
+            let r2 = Math.floor(Math.random() * 255);
+            let g2 = Math.floor(Math.random() * 255);
+            let b2 = Math.floor(Math.random() * 255);
+            crc2.beginPath();
+            crc2.ellipse(x, y, radius / 1.5, radius, 0, Math.PI * 2, 0, false);
+            let gradient = crc2.createLinearGradient(0, 0, 0, radius * 2);
+            gradient.addColorStop(0, "rgb(" + r + "," + g + "," + b + ")");
+            gradient.addColorStop(1, "rgb(" + r2 + "," + g2 + "," + b2 + ")");
+            crc2.fillStyle = gradient;
+            crc2.fill();
+            crc2.moveTo(x, y + radius);
+            crc2.lineTo(x + 3, y + radius + 20);
+            crc2.lineTo(x - 1, y + radius + 40);
+            crc2.lineTo(x - 6, y + radius + 60);
+            crc2.lineTo(x, y + radius + 80);
+            crc2.stroke();
+            crc2.closePath();
+        }
+    }
+    function refresh(_event) {
+        window.location.reload();
+    }
+})(GenerativeKunst || (GenerativeKunst = {}));
+//# sourceMappingURL=script.js.map
